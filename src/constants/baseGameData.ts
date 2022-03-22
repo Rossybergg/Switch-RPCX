acbe6d1ea69d37cb7266d749e2704535a9ef7391
Ross Hunter <ross.hunter@tesco.com>
2022-03-22 10:58:47 +0000

    added games to base game list

--- src/constants/baseGameData.ts
+++ src/constants/baseGameData.ts
@@ -1,6 +1,8 @@
 export const baseGameData = {
-  version: 1,
-  updated: '12/2/2022',
+  listVersion: 1,
+  listUpdated: '12/02/2022',
+  appVersion: 1,
+  appUpdated: '22/03/22',
   newGames: [
     {
       title: 'Kirby and the forgotten land',
@@ -9,6 +11,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/1ddb829d9a730/kirby-and-the-forgotten-land-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Chocobo GP',
@@ -16,6 +19,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/bba9df096eab3/chocobo-gp-cover.cover_300x.jpg',
       year: '2022',
       players: '4 (8 Player Online)',
+      rating: 'N/A'
     },
     {
       title: 'Triangle Strategy',
@@ -23,6 +27,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/f5a0ce0cadcfd/triangle-strategy-cover.cover_300x.jpg',
       year: '2020',
       players: 'Single Player',
+      rating: '9.6'
     },
     {
       title: 'Pokémon Legends: Arceus',
@@ -31,6 +36,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/2889c0391401b/pokemon-legends-arceus-cover.cover_300x.jpg',
       year: '2022',
       players: '2',
+      rating: '8.8'
     },
     {
       title: 'Atelier Sophie 2: The Alchemist of the Mysterious Dream',
@@ -39,6 +45,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/9312d8f5c5e8c/atelier-sophie-2-the-alchemist-of-the-mysterious-dream-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Crystar',
@@ -46,6 +53,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/a1f7032a181d5/crystar-cover.cover_large.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Assassins Creed: The Ezio Collection',
@@ -54,6 +62,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/5b067bb89f885/assassins-creed-the-ezio-collection-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Dynasty Warriors 9: Empires',
@@ -62,6 +71,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/a08616b40d657/dynasty-warriors-9-empires-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Maglam Lord',
@@ -69,6 +79,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/c0434f25b3c8f/maglam-lord-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'WRC 10 The Official Game',
@@ -77,6 +88,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/48faca4958ebd/wrc-10-the-official-game-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
   ],
   allGames: [
@@ -87,6 +99,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/1ddb829d9a730/kirby-and-the-forgotten-land-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Chocobo GP',
@@ -94,6 +107,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/bba9df096eab3/chocobo-gp-cover.cover_300x.jpg',
       year: '2022',
       players: '4 (8 Player Online)',
+      rating: 'N/A'
     },
     {
       title: 'Triangle Strategy',
@@ -101,6 +115,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/f5a0ce0cadcfd/triangle-strategy-cover.cover_300x.jpg',
       year: '2020',
       players: 'Single Player',
+      rating: '9.6'
     },
     {
       title: 'Pokémon Legends: Arceus',
@@ -109,6 +124,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/2889c0391401b/pokemon-legends-arceus-cover.cover_300x.jpg',
       year: '2022',
       players: '2',
+      rating: '8.8'
     },
     {
       title: 'Atelier Sophie 2: The Alchemist of the Mysterious Dream',
@@ -117,6 +133,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/9312d8f5c5e8c/atelier-sophie-2-the-alchemist-of-the-mysterious-dream-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Crystar',
@@ -124,6 +141,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/a1f7032a181d5/crystar-cover.cover_large.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Assassins Creed: The Ezio Collection',
@@ -132,6 +150,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/5b067bb89f885/assassins-creed-the-ezio-collection-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Dynasty Warriors 9: Empires',
@@ -140,6 +159,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/a08616b40d657/dynasty-warriors-9-empires-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Maglam Lord',
@@ -147,6 +167,7 @@ export const baseGameData = {
       cover: 'https://images.nintendolife.com/c0434f25b3c8f/maglam-lord-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'WRC 10 The Official Game',
@@ -155,6 +176,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/48faca4958ebd/wrc-10-the-official-game-cover.cover_300x.jpg',
       year: '2022',
       players: 'Single Player',
+      rating: 'N/A'
     },
     {
       title: 'Mario Kart 8 Deluxe',
@@ -163,6 +185,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/802713810685c/mario-kart-8-deluxe-cover.cover_300x.jpg',
       year: '2017',
       players: '8',
+      rating: '9'
     },
     {
       title: 'The Legend of Zelda: Breath of the Wild',
@@ -171,6 +194,7 @@ export const baseGameData = {
         'https://images.nintendolife.com/15a20bc6c7642/legend-of-zelda-breath-of-the-wild-cover.cover_300x.jpg',
       year: '2017',
       players: 'Single Player',
+      rating: '9.6'
     },
     {
       title: 'The Legend of Zelda: Skyward Sword',
@@ -179,6 +203,286 @@ export const baseGameData = {
         'https://images.nintendolife.com/d0ae00f3e7408/legend-of-zelda-skyward-sword-hd-cover.cover_300x.jpg',
       year: '2021',
       players: 'Single Player',
+      rating: '8.3'
+    },
+    {
+      title: 'Metroid Dread',
+      image: 'metroid',
+      cover:
+        'https://images.nintendolife.com/7c1760474c995/metroid-dread-cover.cover_300x.jpg',
+      year: '2021',
+      players: 'Single Player',
+      rating: '9.3'
+    },
+    {
+      title: 'Xenoblade Chronicles: Definitive Edition',
+      image: 'xenode',
+      cover:
+        'https://images.nintendolife.com/290c527841b45/xenoblade-chronicles-definitive-edition-cover.cover_300x.jpg',
+      year: '2020',
+      players: 'Single Player',
+      rating: '9.3'
+    },
+    {
+      title: 'Super Mario Odyssey',
+      image: 'marioodyssey',
+      cover:
+        'https://images.nintendolife.com/79d7d24eda071/super-mario-odyssey-cover.cover_300x.jpg',
+      year: '2017',
+      players: '2',
+      rating: '9.2'
+    },
+    {
+      title: 'Super Smash Bros. Ultimate',
+      image: 'smashult',
+      cover:
+        'https://images.nintendolife.com/73b5ee31cad64/super-smash-bros-ultimate-cover.cover_300x.jpg',
+      year: '2018',
+      players: '8',
+      rating: '9.2'
+    },
+    {
+      title: 'Dragon Quest XI: Echoes of an Elusive Age',
+      image: 'dragonquestxi',
+      cover:
+        'https://images.nintendolife.com/b3abff89e7617/dragon-quest-xi-s-echoes-of-an-elusive-age---definitive-edition-cover.cover_300x.jpg',
+      year: '2019',
+      players: 'Single Player',
+      rating: '9.1'
+    },
+    {
+      title: 'Super Mario 3D World + Bowser\'s Fury',
+      image: 'mario3d',
+      cover:
+        'https://images.nintendolife.com/af6ce646506f1/super-mario-3d-world-plus-bowsers-fury-cover.cover_300x.jpg',
+      year: '2021',
+      players: '4',
+      rating: '9.1'
+    },
+    {
+      title: 'Fire Emblem: Three Houses',
+      image: 'fireem3',
+      cover:
+        'https://images.nintendolife.com/49f0745dcaa42/fire-emblem-three-houses-cover.cover_300x.jpg',
+      year: '2019',
+      players: 'Single Player',
+      rating: '9'
+    },
+    {
+      title: 'Monster Hunter Rise',
+      image: 'monsterhunterrise',
+      cover:
+        'https://images.nintendolife.com/5310778f213ab/monster-hunter-rise-cover.cover_300x.jpg',
+      year: '2021',
+      players: '4',
+      rating: '9'
+    },
+    {
+      title: 'Sonic Mania Plus',
+      image: 'sonicmania',
+      cover:
+        'https://images.nintendolife.com/1b8c9953c7de1/sonic-mania-plus-cover.cover_300x.jpg',
+      year: '2018',
+      players: '2',
+      rating: '8.9'
+    },
+    {
+      title: 'Animal Crossing: New Horizons',
+      image: 'acnewhorizons',
+      cover:
+        'https://images.nintendolife.com/bf43ef284f030/animal-crossing-new-horizons-cover.cover_300x.jpg',
+      year: '2020',
+      players: 'Single Player (8 Online)',
+      rating: '8.9'
+    },
+    {
+      title: 'The Great Ace Attorney Chronicles',
+      image: 'greatace',
+      cover:
+        'https://images.nintendolife.com/3def5cf3d9d83/great-ace-attorney-chronicles-cover.cover_300x.jpg',
+      year: '2021',
+      players: 'Single Player',
+      rating: '8.9'
+    },
+    {
+      title: 'Shin Megami Tensei V',
+      image: 'tenseiv',
+      cover:
+        'https://images.nintendolife.com/302b9d8ae0cce/shin-megami-tensei-v-cover.cover_300x.jpg',
+      year: '2021',
+      players: 'Single Player',
+      rating: '8.8'
+    },
+    {
+      title: 'Danganronpa Decadence',
+      image: 'danganronpa',
+      cover:
+        'https://images.nintendolife.com/42996599f7db5/danganronpa-decadence-cover.cover_300x.jpg',
+      year: '2021',
+      players: 'Single Player',
+      rating: '8.8'
+    },
+    {
+      title: 'Xenoblade Chronicles 2: Torna - The Golden Country',
+      image: 'xenoblade2torna',
+      cover:
+        'https://images.nintendolife.com/1adc6cd91ea83/xenoblade-chronicles-2-torna---the-golden-country-cover.cover_300x.jpg',
+      year: '2018',
+      players: 'Single Player',
+      rating: '8.8'
+    },
+    {
+      title: 'Xenoblade Chronicles 2',
+      image: 'xenoblade2',
+      cover:
+        'https://images.nintendolife.com/4c08b3ab1227b/xenoblade-chronicles-2-cover.cover_300x.jpg',
+      year: '2017',
+      players: 'Single Player',
+      rating: '8.8'
+    },
+    {
+      title: 'The Witcher 3: Wild Hunt - Complete Edition',
+      image: 'witcher3',
+      cover:
+        'https://images.nintendolife.com/c4503720f5b69/witcher-3-wild-hunt---complete-edition-cover.cover_300x.jpg',
+      year: '2019',
+      players: 'Single Player',
+      rating: '8.8'
+    },
+    {
+      title: 'Luigi\'s Mansion 3',
+      image: 'luigismansion3',
+      cover:
+        'https://images.nintendolife.com/0312411e76a66/luigis-mansion-3-cover.cover_300x.jpg',
+      year: '2019',
+      players: '8',
+      rating: '8.8'
+    },
+    {
+      title: 'Splatoon 2',
+      image: 'splatoon2',
+      cover:
+        'https://images.nintendolife.com/27de74a6ad71a/splatoon-2-cover.cover_300x.jpg',
+      year: '2017',
+      players: '8',
+      rating: '8.7'
+    },
+    {
+      title: 'The Legend Of Heroes: Trails of Cold Steel IV',
+      image: 'legendofheroes',
+      cover:
+        'https://images.nintendolife.com/56595dd2664cc/legend-of-heroes-trails-of-cold-steel-iv-cover.cover_300x.jpg',
+      year: '2021',
+      players: 'Single Player',
+      rating: '8.7'
+    },
+    {
+      title: 'Donkey Kong Country: Tropical Freeze',
+      image: 'tropicalfreeze',
+      cover:
+        'https://images.nintendolife.com/68b153f4aa78f/donkey-kong-country-tropical-freeze-cover.cover_300x.jpg',
+      year: '2018',
+      players: '2',
+      rating: '8.7'
+    },
+    {
+      title: 'Atelier Ryza 2: Lost Legends & The Secret Fairy',
+      image: 'atelierryza2',
+      cover:
+        'https://images.nintendolife.com/305bd60aace8e/atelier-ryza-2-lost-legends-and-the-secret-fairy-cover.cover_300x.jpg',
+      year: '2021',
+      players: 'Single Player',
+      rating: '8.7'
+    },
+    {
+      title: 'Beyonetta 2',
+      image: 'beyonetta2',
+      cover:
+        'https://images.nintendolife.com/5a06310100b22/bayonetta-2-cover.cover_300x.jpg',
+      year: '2018',
+      players: '2',
+      rating: '8.7'
+    },
+    {
+      title: 'Pikmin 3 Deluxe',
+      image: 'pikmin3',
+      cover:
+        'https://images.nintendolife.com/f61769b895407/pikmin-3-deluxe-cover.cover_300x.jpg',
+      year: '2020',
+      players: '2',
+      rating: '8.6'
+    },
+    {
+      title: 'Mario Party Superstars',
+      image: 'mariopartysuperstars',
+      cover:
+        'https://images.nintendolife.com/778a5c46b43fa/mario-party-superstars-cover.cover_300x.jpg',
+      year: '2021',
+      players: '4',
+      rating: '8.6'
+    },
+    {
+      title: 'Super Mario 3D All-Stars',
+      image: 'marioallstars',
+      cover:
+        'https://images.nintendolife.com/2f68e4c2d5fee/super-mario-3d-all-stars-cover.cover_300x.jpg',
+      year: '2020',
+      players: '2',
+      rating: '8.6'
+    },
+    {
+      title: 'Bioshock: The Collection',
+      image: 'bioshock',
+      cover:
+        'https://images.nintendolife.com/6e4f93aea158d/bioshock-the-collection-cover.cover_300x.jpg',
+      year: '2020',
+      players: 'Single Player',
+      rating: '8.6'
+    },
+    {
+      title: 'The Elder Scrolls V : Skyrim',
+      image: 'skyrim',
+      cover:
+        'https://images.nintendolife.com/445053e1716c9/elder-scrolls-v-skyrim-cover.cover_300x.jpg',
+      year: '2017',
+      players: 'Single Player',
+      rating: '8.5'
+    },
+    {
+      title: 'Doom',
+      image: 'doom',
+      cover:
+        'https://images.nintendolife.com/a387aa04f7772/doom-cover.cover_300x.jpg',
+      year: '2017',
+      players: 'Single Player (12 Online)',
+      rating: '8.5'
+    },
+    {
+      title: 'Super Mario Maker 2',
+      image: 'mariomaker2',
+      cover:
+        'https://images.nintendolife.com/8ea5ee2768ec7/super-mario-maker-2-cover.cover_300x.jpg',
+      year: '2019',
+      players: '4',
+      rating: '8.5'
+    },
+    {
+      title: 'Hyrule Warriors: Age Of Calamity',
+      image: 'ageofcalamity',
+      cover:
+        'https://images.nintendolife.com/467baa022265f/hyrule-warriors-age-of-calamity-cover.cover_300x.jpg',
+      year: '2020',
+      players: '2',
+      rating: '8.5'
+    },
+    {
+      title: 'Dying Light Platinum Edition',
+      image: 'dyinglight',
+      cover:
+        'https://images.nintendolife.com/e60ba181459c7/dying-light-platinum-edition-cover.cover_large.jpg',
+      year: '2021',
+      players: '5',
+      rating: '8.5'
     },
   ],
 };
